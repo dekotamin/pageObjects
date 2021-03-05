1618734666c2d3c32b177a63f2806eeaed8d8fbb
Alina <nyrete@gmail.com>
2021-03-06 01:06:40 +0500

    Сборка болванки проекта

---
+++ src/main/java/ru/netology/LoginPageV1.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class LoginPageV1 {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id=login] input").setValue(info.getLogin());
+        $("[data-test-id=password] input").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+        return new VerificationPage();
+    }
+}
---
+++ src/main/java/ru/netology/VerificationPage.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+}}
